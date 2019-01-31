8adb4220bededeb1832532810d2ce530a2cb4af0
gabegaerlan <ggaerlan@csumb.edu>
2019-01-31 02:39:54 -0800

    Flix App_CST495

---
+++ FlixApp/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  FlixApp
+//
+//  Created by Gabe Gaerlan on 1/31/19.
+//  Copyright © 2019 Gabe Gaerlan. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
