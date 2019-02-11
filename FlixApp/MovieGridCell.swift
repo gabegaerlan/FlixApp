f6e2aeec328c9931a066e92956cf3c2231a11393
gabegaerlan <ggaerlan@csumb.edu>
2019-02-11 14:02:07 -0800

    Added More Features

---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by Gabe Gaerlan on 2/11/19.
+//  Copyright © 2019 Gabe Gaerlan. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+}
